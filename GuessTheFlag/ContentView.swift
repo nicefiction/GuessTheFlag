2fa9ea4013db7849f97201af9ec1b68009c1a1cf
Olivier Van hamme <nicefiction@gmail.com>
2021-04-04 08:40:01 +0200

    Stacks up the buttons .

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -1,21 +1,55 @@
-//
 //  ContentView.swift
-//  GuessTheFlag
-//
-//  Created by Olivier Van hamme on 03/04/2021.
-//
+// LIBRARIES :
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    var correctAnswer = Int.random(in: 0...2)
+    let countries: [String] = [
+        
+        "Estonia" , "France" , "Germany" , "Ireland" , "Italy" , "Monaco" , "Nigeria" , "Poland" , "Russia" , "Spain" , "UK" , "USA"
+    ]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Color
+                .blue
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing : 30) {
+                VStack {
+                    Text("Guess the Flag of")
+                    Text("\(countries[correctAnswer])")
+                }
+                .foregroundColor(Color.white)
+                
+                ForEach(0..<3) { (flagIndex: Int) in
+                    Button(action : {
+                        print("Flag \(countries[flagIndex]) was tapped .")
+                    }) {
+                        Image(self.countries[flagIndex])
+                            .renderingMode(.original) // The renderingMode(.original) modifier tells SwiftUI to render the original image pixels , rather than trying to recolor them as a button .
+                    }
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        ContentView()
+        
+        ContentView().previewDevice("iPhone 12 Pro")
     }
 }
